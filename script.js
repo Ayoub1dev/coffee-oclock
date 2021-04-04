85015d4ef80f1821364a6994fb14fd1dcbb6b6d5
Ayoub1dev <enasa6608@gmail.com>
2021-04-04 13:53:26 +0100

    Added rotaion functionality to footer buttons

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // coffe table animation
 
-
 const CUPS_PARENT = document.querySelector(".coffee-table-container");
-            // Left the parent alone so I can easily add a style to it later
+// Left the parent alone so I can easily add a style to it later
 const CUPS_LIST = CUPS_PARENT.children;
 
 // Previous and Next buttons
- const BEFORE = document.querySelector('.before');
- const AFTER = document.querySelector('.after');
+const BEFORE = document.querySelector(".before");
+const AFTER = document.querySelector(".after");
 // below there is the code to rotate coffee table when the coffee mugs are clicked
 
 /*
@@ -70,54 +69,40 @@ for (let i = 0; i < CUPS_LIST.length; i++) {
 }
 */
 
-
 // the below for looop is to rotate the mugs when clicked
-for( let i=0; i<CUPS_LIST.length; i++){
-    CUPS_LIST[i].addEventListener("click", function(){
-        
+for (let i = 0; i < CUPS_LIST.length; i++) {
+    CUPS_LIST[i].addEventListener("click", function () {
         id = CUPS_LIST[i].id;
 
-        switch (id){
-            // the role of this switch statement is to give the 'transform' 
+        switch (id) {
+            // the role of this switch statement is to give the 'transform'
             // property to the clicked mug.
 
             case `cup-${i}`:
-                console.log('switch working')
+                console.log("switch working");
 
-                CUPS_PARENT.style.removeProperty('transform');
-                CUPS_PARENT.style.transform = `rotate(${i * 45}deg)`; 
-                    // The  template literal above is to change the turning degree  
-                    // according to the clicked id. This way it will always rotate clock-wise.
+                CUPS_PARENT.style.removeProperty("transform");
+                CUPS_PARENT.style.transform = `rotate(${i * 45}deg)`;
+                // The  template literal above is to change the turning degree
+                // according to the clicked id. This way it will always rotate clock-wise.
 
-                console.log(`rotation ${i*45} deg`)
+                console.log(`rotation ${i * 45} deg`);
         }
-        
-    })
+    });
 }
 
 // The code below is for the footer buttons to rotate the coffe table.
 
+let angle = 0;
+
+BEFORE.addEventListener("click", function () {
+    angle -= 45;
+    CUPS_PARENT.style.transform = `rotate(${angle}deg)`;
+});
+
+AFTER.addEventListener("click", function () {
+    angle += 45;
+    CUPS_PARENT.style.transform = `rotate(${angle}deg)`;
+});
+
 // Still in progress!!
-let sub = -45;
-BEFORE.addEventListener('click', function(){
-    
-    CUPS_PARENT.style.transform = `rotate(${sub}deg)`; 
-    sub -= 45;
-    console.log('1111');
-    
-})
-
-let add = 45;
-AFTER.addEventListener('click', function(){
-    
-    CUPS_PARENT.style.transform = `rotate(${add}deg)`; 
-    add += 45;
-    console.log('222');
-})
-
-
-
-
-
-
-
