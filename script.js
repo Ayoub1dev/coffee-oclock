5e803cea1b55931f66c88e2257f5d02eece925e1
Ayoub1dev <enasa6608@gmail.com>
2021-04-08 12:16:03 +0100

    Finished container properties section

--- script.js
+++ script.js
@@ -69,6 +69,104 @@ for (let i = 0; i < CUPS_LIST.length; i++) {
 }
 */
 
+// The code below is to change the coffee properties container content respectively to what
+// coffee mug was clicked.
+const coffeProps = {
+    // the key name is the same as the id of the coffee mugs on the coffee table
+    "cup-0": {
+        title: "AMERICANO",
+        description: "text",
+        image: `<img src="images/americano.png"
+                        alt="americano coffee mug"
+                         style="
+                           display: block;
+                            margin: auto;
+                            width: 230px;"
+                    />`,
+    },
+    "cup-1": {
+        title: " ESPRESSO MACHIATO",
+        description: "text",
+        image: `<img src="images/espresso-machiato.png"
+                        alt="ESPRESSO MACHIATO"
+                         style="
+                           display: block;
+                            margin: auto;
+                            width: 230px;"
+                    />`,
+    },
+    "cup-2": {
+        title: "ESPRESSO",
+        description: "text",
+        image: `<img src="images/espresso.png"
+                        alt="espresso coffee "
+                         style="
+                           display: block;
+                            margin: auto;
+                            width: 230px;"
+                    />`,
+    },
+    "cup-3": {
+        title: "CAFE MOCHA",
+        description: "text",
+        image: `<img src="images/cafe-mocha.png"
+                        alt="cafe mocha"
+                         style="
+                           display: block;
+                            margin: auto;
+                            width: 230px;"
+                    />`,
+    },
+    "cup-4": {
+        title: "LATTE",
+        description: "text",
+        image: `<img src="images/latte.png"
+                        alt="latte coffee "
+                         style="
+                           display: block;
+                            margin: auto;
+                            width: 230px;"
+                    />`,
+    },
+    "cup-5": {
+        title: "CAPUCCINO",
+        description: "text",
+        image: `<img src="images/cappuccino.png"
+                        alt="cappuccino"
+                         style="
+                           display: block;
+                            margin: auto;
+                            width: 230px;"
+                    />`,
+    },
+    "cup-6": {
+        title: "CAFE BREVE",
+        description: "text",
+        image: `<img src="images/cafe-breve.png"
+                        alt="CAFE BREVE"
+                         style="
+                           display: block;
+                            margin: auto;
+                            width: 230px;"
+                    />`,
+    },
+    "cup-7": {
+        title: "TURKISH COFFEE",
+        description: "tt",
+        image: `<img src="images/turkish-coffee.png"
+        alt="turkish coffee"
+         style="
+           display: block;
+            margin: auto;
+            width: 230px;"
+    />`,
+    },
+};
+
+let coffeeTitle = document.querySelector(".coffee-title");
+let coffeeDescription = document.querySelector(".coffee-description");
+let coffeImage = document.querySelector(".coffee-image-container");
+
 // the below for looop is to rotate the mugs when clicked
 let angle = 0;
 
@@ -87,6 +185,10 @@ for (let i = 0; i < CUPS_LIST.length; i++) {
                 CUPS_PARENT.style.transform = `rotate(${i * 45}deg)`;
                 // The  template literal above is to change the turning degree
                 // according to the clicked id. This way it will always rotate clock-wise.
+                coffeeTitle.innerHTML = coffeProps[`cup-${i}`].title;
+                coffeeDescription.innerHTML =
+                    coffeProps[`cup-${i}`].description;
+                coffeImage.innerHTML = coffeProps[`cup-${i}`].image;
 
                 angle = i * 45;
                 // the anle variable will be updated to what angle the div is so when the footer
