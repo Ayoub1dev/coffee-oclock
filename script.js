f047022ffd028eec384f8ba88780e335ad691f0f
Ayoub1dev <68614024+Ayoub1dev@users.noreply.github.com>
2021-04-14 16:36:23 +0100

    Update script.js
    Removed Old code

--- script.js
+++ script.js
@@ -9,65 +9,9 @@ const BEFORE = document.querySelector(".before");
 const AFTER = document.querySelector(".after");
 // below there is the code to rotate coffee table when the coffee mugs are clicked
 
-/*
 
-for (let i = 0; i < CUPS_LIST.length; i++) {
-    // the for loop below was the first try to make the mugs rotate towards the properties table
-    // when clicked. My new shorter version is below this lOoOong for loop.
+// Old code (1) was replaced by better simplified code - to check it out got to oldCode.js file.
 
-    CUPS_LIST[i].addEventListener("click", function () {
-         console.log(CUPS_LIST[i].id );
-         
-         id = CUPS_LIST[i].id;
-         
-         switch (id){
-            case 'cup-1':
-                
-                CUPS_PARENT.style.removeProperty('transform');
-                CUPS_PARENT.style.transform = `rotate(90deg)`; 
-                break;
-
-            case 'cup-2':
-                
-                CUPS_PARENT.style.removeProperty('transform');
-                CUPS_PARENT.style.transform = `rotate(45deg)`;
-                
-                break;
-            case 'cup-3':
-                
-                CUPS_PARENT.style.removeProperty('transform');
-                CUPS_PARENT.style.transform = `rotate(0deg)`; 
-                break;
-
-            case 'cup-4':
-                CUPS_PARENT.style.removeProperty('transform');
-                CUPS_PARENT.style.transform = `rotate(315deg)`;
-                break;
-
-            case 'cup-5':
-                CUPS_PARENT.style.removeProperty('transform');
-                CUPS_PARENT.style.transform = `rotate(270deg)`; 
-                break;
-
-            case 'cup-6':
-                CUPS_PARENT.style.removeProperty('transform');
-                CUPS_PARENT.style.transform = `rotate(225deg)`;
-                break;
-
-            case 'cup-7':
-                CUPS_PARENT.style.removeProperty('transform');
-                CUPS_PARENT.style.transform = `rotate(180deg)`; 
-                break;
-
-            case 'cup-8':
-                CUPS_PARENT.style.removeProperty('transform');
-                CUPS_PARENT.style.transform = `rotate(135deg)`;
-                break;
-        }
-         
-    });
-}
-*/
 
 // The code below is to change the coffee properties container content respectively to what
 // coffee mug was clicked.
